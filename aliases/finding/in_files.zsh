ded429486940e34f66dbf8d00169846d1b2d0208
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-03-01 19:43:02 +0100

    fixed rg alias that only allows searching files matching X instead of forbidding searching files that match X

--- aliases/finding/in_files.zsh
+++ aliases/finding/in_files.zsh
@@ -2,4 +2,4 @@
 
 alias brg="rg --glob '!/proc/*' --glob '!/sys/*' --glob '!/dev/*' --glob '!/run/*' --glob '!/@swap/*'" # [B]ase [R]ip [G]aep
 
-alias rg="rg --glob '!/proc/*' --glob '!/sys/*' --glob '!/dev/*' --glob '!/run/*' --glob '!/@swap/*' --glob '/.snapshots/*' --glob '/home/.snapshots/*' --glob '!/mnt/*' --hidden --no-ignore" # [R]ip [G]rep
+alias rg="rg --glob '!/proc/*' --glob '!/sys/*' --glob '!/dev/*' --glob '!/run/*' --glob '!/@swap/*' --glob '!/.snapshots/*' --glob '!/home/.snapshots/*' --glob '!/mnt/*' --hidden --no-ignore" # [R]ip [G]rep
