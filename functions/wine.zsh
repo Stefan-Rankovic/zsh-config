6e0987239b4dd4ff7ac90d38ecb61a67e288c4cb
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-03-09 22:49:18 +0100

    added colors to the wine function

--- functions/wine.zsh
+++ functions/wine.zsh
@@ -2,14 +2,24 @@
 
 # [WINE]
 wine() {
-	echo -e "\e[33mWARNING: No prefix specified! Wine state will be reset after it quits!\e[0m"
-	echo -n "Continue? (y/N) "
+	local RESET="\e[0m"
+	local RED="\e[31m"
+	local GREEN="\e[32m"
+	local YELLOW="\e[33m"
+	local BLUE="\e[34m"
+	local MAGENTA="\e[35m"
+	printf "${YELLOW}WARNING: No prefix specified! Wine state will be reset after it quits!\n"
+	printf "Continue?${RESET} (${GREEN}y${RESET}/${RED}N${RESET}) "
 	read confirm
 	[[ "$confirm" =~ ^[Yy]$ ]] || return 1
 
 	command wine "$@"
 
-	echo "Cleaning up ~/.wine..."
+	for i in 5 4 3 2 1; do
+		printf "\r${BLUE}Cleaning up ~/.wine in ${MAGENTA}$i${BLUE} seconds...${RESET}"
+		sleep 1
+	done
+	printf "\r${BLUE}Cleaning up ~/.wine...${RESET}                                                 \n"
 	rm -rf ~/.wine/
 	wineboot --init
 }
