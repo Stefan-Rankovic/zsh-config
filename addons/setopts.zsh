3b44cf00419639aaaf7f281176ccbf5441a68db1
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-03-09 23:04:27 +0100

    added history, spell correction, and more setopts

---
+++ addons/setopts.zsh
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+# See `man zshoptions` for documentation on these.
+
+setopt AUTO_CD
+setopt CORRECT
+setopt EXTENDED_HISTORY
+setopt INC_APPEND_HISTORY_TIME
