81bb2226deee4a0921271198a148fdd7fd8323a2
Stefan-Rankovic <stefi.rankovic@proton.me>
2026-03-14 14:17:25 +0100

    fixed pin alias comment

--- aliases/packages.zsh
+++ aliases/packages.zsh
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
-alias pin="pm -S" # [P]ackage [I]nstall
+alias pin="pm -S" # [P]ackage [IN]stall
 alias prm="pm -Rns" # [P]ackage [R]e[M]ove
 alias psr="pm -Ss" # [P]ackage [S]ea[R]ch
 alias prmc="spm -Sc" # [P]ackage [R]e[M]ove [C]ache
